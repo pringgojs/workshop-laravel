ae20299161394aaadf146125b1d94ca5b8870448
pringgojs <odyinggo@gmail.com>
2018-05-04 13:20:50 +0700

    rename button

--- resources/views/program-study/edit.blade.php
+++ resources/views/program-study/edit.blade.php
@@ -21,7 +21,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Submit
                                 </button>
                             </div>
                         </div>
